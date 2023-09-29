7fd781b6ebe67de322c27c28f4511bf0dff4f051
jess <120062852+jessabc@users.noreply.github.com>
2023-09-29 19:01:28 +1000

    refactor

--- src/components/DisplayWordDefinition.vue
+++ src/components/DisplayWordDefinition.vue
@@ -8,16 +8,16 @@ import Meaning from './Meaning.vue';
       }
   })
 
-  const phonetic = props.word.phonetics[0].audio
+  const phonetic = props?.word?.phonetics[0]?.audio
   const audio = new Audio(phonetic)
 
 </script>
 
 <template>
  
- <div class='flex items-center mb-10'>
+ <div class='flex items-center mb-10 '>
     <div >
-     <h2 class="font-bold text-5xl  my-5">{{ word.word }}</h2>
+     <h2 class="font-bold text-5xl  my-5 dark:text-zinc-50">{{ word.word }}</h2>
     <p class="text-purple-500 text-xl '">{{ word.phonetic }}</p> 
     </div>
 
@@ -37,8 +37,8 @@ import Meaning from './Meaning.vue';
 
     <hr/>
     <div className='flex gap-3 pt-5 pb-10 items-end flex-wrap '> 
-      <p className='text-zinc-500 text-sm'>Source: </p>
-      <a :href="word.sourceUrls[0]" target="_blank" class='flex gap-3 '>{{ word.sourceUrls[0] }}
+      <p className='text-zinc-500 text-sm dark:text-zinc-400'>Source: </p>
+      <a :href="word.sourceUrls[0]" target="_blank" class='flex gap-3 dark:text-zinc-50'>{{ word.sourceUrls[0] }}
         <img src="../assets/icon-new-window.svg" alt="new window icon" />
       </a>
     </div>
