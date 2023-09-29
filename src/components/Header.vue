d2816bd87ab7fa44bd7e7de25bf4a1d970a7afae
jess <120062852+jessabc@users.noreply.github.com>
2023-09-29 15:12:29 +1000

    add header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,15 @@
-<template>
-    <div>
-
-    </div>
-</template>
-
 <script setup>
+import Toggle from './Toggle.vue';
 
 </script>
+
+<template>
+    <div class="bg-yellow-500 dark:bg-slate-700 flex justify-between mt-5">
+        <img src="../assets/logo.svg" alt="">
+           <Toggle/>
+    </div>
+ 
+</template>
+
+
 
