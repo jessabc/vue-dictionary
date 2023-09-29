290ebe57f1c2754928d476673435d76838f9f5e2
jess <120062852+jessabc@users.noreply.github.com>
2023-09-29 18:59:52 +1000

    fix fetch api

--- src/store/query.js
+++ src/store/query.js
@@ -10,12 +10,17 @@ export const useQueryStore = defineStore('query', () => {
         definition: null
     })
 
-    const getWordDefinition = async () => {
+    const getWordDefinition = async (wordVal) => {
         query.definition = null
         query.errMsg = null
         query.loading = true
+        console.log('argh',query.text)
         try{
-            const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${query.text}`)
+            console.log('ggggr',query.text)
+
+            const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${query.text ? query.text : wordVal}`)
+            
+            console.log(response)
             query.definition = [response.data[0]]
         }catch(err) {
             query.errMsg = err.response.data
@@ -27,3 +32,4 @@ export const useQueryStore = defineStore('query', () => {
    
     return { query, getWordDefinition }
 })
+
